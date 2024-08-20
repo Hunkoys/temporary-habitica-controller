07ec4e16384b2f7e28223b70902ecd52a9bd1ae3
hunkoys <hunkyminik@gmail.com>
2024-08-19 23:47:54 -0700

    chore: Update package name and add PWA support

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import pwa from 'next-pwa';
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== 'development',
+  },
+};
+
+const withPWA = pwa({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+});
+
+export default withPWA(nextConfig);
