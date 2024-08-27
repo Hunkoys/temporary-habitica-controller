7f06a119ff688f553539201c0a38caa6dd9a5400
hunkoys <hunkyminik@gmail.com>
2024-08-27 12:49:53 -0700

    prisma setup, split env files

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,26 @@
-export default function Home() {
-  return <main>JHelo</main>;
+import db from '@/prisma/db';
+
+export default async function Home() {
+  return (
+    <main>
+      <form
+        action={async (formData: FormData) => {
+          'use server';
+          const email = formData.get('email');
+          const name = formData.get('name');
+          const password = formData.get('password');
+          if (typeof email !== 'string' || typeof name !== 'string' || typeof password !== 'string') {
+            return;
+          }
+          const response = await db.user.create({ data: { email, name, password } });
+        }}
+        className="flex flex-col gap-1"
+      >
+        <input type="email" name="email" placeholder="email" required />
+        <input type="text" name="name" placeholder="name" />
+        <input type="password" name="password" placeholder="password" />
+        <button type="submit">Submit</button>
+      </form>
+    </main>
+  );
 }
