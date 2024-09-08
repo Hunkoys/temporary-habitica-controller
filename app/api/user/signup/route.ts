f8d737bc9d1c603d75fb60097cf301e9f8c5f115
hunkoys <hunkyminik@gmail.com>
2024-09-08 04:22:46 -0700

    Setup ngrok for local testing. Webhook now working

--- app/api/user/signup/route.ts
+++ app/api/user/signup/route.ts
@@ -1,46 +1,44 @@
 import { headers } from 'next/headers';
 import { NextResponse, type NextRequest } from 'next/server';
 import { Webhook } from 'svix';
-
-const secret = process.env.SVIX_SECRET || '';
+import { WebhookEvent } from '@clerk/nextjs/server';
 
 export async function POST(request: NextRequest) {
-  const payload = await request.json();
-  const payloadString = JSON.stringify(payload);
+  const SECRET = process.env.SVIX_SECRET;
+
+  if (!SECRET) {
+    throw new Error('Failed to get the secret key from the environment variables');
+  }
+
+  // Get the headers
   const headerPayload = headers();
+
   const svixId = headerPayload.get('svix-id');
   const svixIdTimeStamp = headerPayload.get('svix-timestamp');
   const svixSignature = headerPayload.get('svix-signature');
 
   if (!svixId || !svixIdTimeStamp || !svixSignature) {
-    console.log('svixId', svixId);
-    console.log('svixIdTimeStamp', svixIdTimeStamp);
-    console.log('svixSignature', svixSignature);
-    return new NextResponse('Error occured', {
+    console.error('Error in Svix headers:', { svixId, svixIdTimeStamp, svixSignature });
+    return new NextResponse('Error occured -- no svix headers', {
       status: 400,
     });
   }
 
-  const svixHeaders = {
-    'svix-id': svixId,
-    'svix-timestamp': svixIdTimeStamp,
-    'svix-signature': svixSignature,
-  };
+  const payload = await request.json();
+  const body = JSON.stringify(payload);
 
-  if (!secret) {
-    console.log('SVIX_SECRET is not found');
-    return new NextResponse('Error occured', {
-      status: 500,
-    });
-  }
+  const wh = new Webhook(SECRET);
 
-  const wh = new Webhook(secret);
-  let evt;
+  let evt: WebhookEvent;
 
   try {
-    evt = wh.verify(payloadString, svixHeaders);
+    evt = wh.verify(body, {
+      'svix-id': svixId,
+      'svix-timestamp': svixIdTimeStamp,
+      'svix-signature': svixSignature,
+    }) as WebhookEvent;
   } catch (err) {
-    console.log("Error: Can't verify the webhook");
+    console.error('Error verifying webhook:', err);
     return new NextResponse('Error occured', {
       status: 400,
     });
