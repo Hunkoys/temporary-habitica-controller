2ed3ff979dcd2238afffaf051d52a3eac2fb6986
Dominic Fecundo <hunkyminik@gmail.com>
2025-01-31 01:55:40 -0800

    Fixed Sign in mobile not popup

--- app/_components/elements/login/SignInCard.tsx
+++ app/_components/elements/login/SignInCard.tsx
@@ -1,28 +1,13 @@
 "use client";
 
 import CommonButton from "@/app/_components/elements/CommonButton";
-import {
-  Card,
-  CardBody,
-  CardFooter,
-  CardHeader,
-  Divider,
-} from "@heroui/react";
-import { useCallback, useEffect, useState } from "react";
 import clerkLogo from "@/app/_components/elements/login/clerk-logo-light-accent.svg";
-import Image from "next/image";
 import { SignInButton, useUser } from "@clerk/nextjs";
-import clsx from "clsx";
+import { Card, CardBody, CardFooter, Divider } from "@heroui/react";
+import Image from "next/image";
 
 export default function SignInCard() {
-  const [isLoading, setIsLoading] = useState(false);
-  const onClick = useCallback(() => setIsLoading(true), []);
-
   const user = useUser();
-
-  useEffect(() => {
-    setIsLoading(false);
-  }, [isLoading]);
 
   return (
     <section className="h-full flex flex-col items-center justify-center">
@@ -32,21 +17,13 @@ export default function SignInCard() {
             <p className="text-gray-400">Welcome to</p>
             <h1 className="text-2xl font-bold text-secondary-400">Habitica+</h1>
           </div>
-          <CommonButton
-            as={"div"}
-            color="primary"
-            isDisabled={!user.isLoaded}
-            isLoading={isLoading}
-            onClick={onClick}
-          >
-            {user.isLoaded ? (
-              <SignInButton mode="modal">
-                {isLoading ? "Signing In..." : "Sign In"}
-              </SignInButton>
-            ) : (
-              ""
-            )}
-          </CommonButton>
+          {user.isLoaded ? (
+            <SignInButton mode="modal">
+              <CommonButton color="primary">Sign In</CommonButton>
+            </SignInButton>
+          ) : (
+            <CommonButton isDisabled>Sign In</CommonButton>
+          )}
         </CardBody>
         <Divider className="w-full" />
         <CardFooter className="text-sm text-foreground-400 flex justify-center align-baseline gap-1 mt-1 p-0">
