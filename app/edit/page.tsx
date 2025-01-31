bb17e0c52366d10ee07f751a2fc886d51daa1b2a
Dominic Fecundo <hunkyminik@gmail.com>
2025-01-31 02:42:04 -0800

    Used new Link

--- app/edit/page.tsx
+++ app/edit/page.tsx
@@ -1,15 +1,12 @@
 import CommonButton from "@/app/_components/elements/CommonButton";
+import CommonLink from "@/app/_components/elements/CommonLink";
 import Link from "next/link";
 
 export default function EditPage() {
   return (
     <div>
-      <CommonButton>
-        <Link href="/edit/egos">Egos</Link>
-      </CommonButton>
-      <CommonButton>
-        <Link href="/edit/tasks">Tasks</Link>
-      </CommonButton>
+      <CommonLink href="/edit/egos">Egos</CommonLink>
+      <CommonLink href="/edit/stats">Stats</CommonLink>
     </div>
   );
 }
